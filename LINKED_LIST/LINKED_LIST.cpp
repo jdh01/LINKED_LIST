800746e4017d5b26f99eb153df0d01f376bad74c
jdh01 <jdh23@kent.ac.uk>
2022-11-10 17:23:40 +0000

    Implemented a iterative list reverse function

--- LINKED_LIST/LINKED_LIST.cpp
+++ LINKED_LIST/LINKED_LIST.cpp
@@ -1,4 +1,3 @@
-//Time Complexity O(n)
 #include<iostream>
 
 struct Node
@@ -9,13 +8,13 @@ struct Node
 
 void InsertAtStart(Node** pointerToHead, int data)
 {
-	Node* temp = new Node; //malloc returns void pointer, so we type case to Node*
+	Node* temp = new Node;
 	temp->data = data;
 	temp->link = *pointerToHead; //remember to dereference
 	*pointerToHead = temp;
 }
 
-void Insert(Node** head, int data, int position)
+Node* Insert(Node** head, int data, int position)
 {
 	Node* temp1 = new Node;
 	temp1->data = data;
@@ -23,7 +22,7 @@ void Insert(Node** head, int data, int position)
 	if (position == 1) {
 		temp1->link = *head;
 		*head = temp1;
-		return;
+		return *head;
 	}
 	Node* temp2 = *head;
 	for (int i = 0; i < position-2; i++) {
@@ -31,6 +30,26 @@ void Insert(Node** head, int data, int position)
 	}
 	temp1->link = temp2->link;
 	temp2->link = temp1;
+}
+
+void Delete(Node* head, int position)
+{
+	Node* temp1 = head;
+	if (position == 1) //Case of deleting the head
+	{
+		head = temp1->link;
+		delete temp1;
+		return;
+	}
+	
+	temp1 = head->link;
+	for (int i = 0; i < position - 2; i++)
+	{
+		temp1 = temp1->link;
+	}
+	Node* temp2 = temp1->link;
+	temp1->link = temp2 ->link;
+	delete temp2;
 }
 
 void Print(Node* head)
@@ -56,27 +75,52 @@ void ListCount(Node* head)
 	std::cout << "The list is " << count << " elements long." << std::endl;
 }
 
+Node* IterativeReverse(Node** head)
+{
+	Node* prev, *current, *next;
+	current = *head;
+	prev = NULL;
+	while (current != NULL)
+	{
+		next = current->link;
+		current->link = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return *head;
+}
+
 int main()
 {
 	Node* head = NULL;
 	int n, x, j;
 
-	std::cout << "The list currently has no data." << std::endl;
-	std::cout << "Enter a number into the list: ";
-	std::cin >> x;
-	Insert(&head, x, 1); //List name, Data, Position
+	//std::cout << "The list currently has no data." << std::endl;
+	//std::cout << "Enter a number into the list: ";
+	//std::cin >> x;
+	Insert(&head, 1, 1); //List name, Data, Position
+	//Print(head);
+
+	//std::cout << "Enter a new number into the list: ";
+	//std::cin >> x;
+	//std::cout << "What index in the list?: ";
+	//std::cin >> j;
+	////InsertAtStart(&head, x);
+
+	Insert(&head, 2, 2); //List name, Data, Position
+	Insert(&head, 3, 3); //List name, Data, Position
+	Insert(&head, 4, 4); //List name, Data, Position
 	Print(head);
+	ListCount(head);
+	Insert(&head, 5, 5); //List name, Data, Position
+	Insert(&head, 6, 6); //List name, Data, Position
+	Insert(&head, 7, 7); //List name, Data, Position
+	Print(head);
+	ListCount(head);
+	IterativeReverse(&head);
+	//Delete(head, 1); //Counting from 0
 
-	while (1)
-	{
-		std::cout << "Enter a new number into the list: ";
-		std::cin >> x;
-		std::cout << "What index in the list?: ";
-		std::cin >> j;
-		//InsertAtStart(&head, x);
-
-		Insert(&head, x, j); //List name, Data, Position
-		ListCount(head);
-		Print(head);
-	}
+	Print(head);
+	std::cin.get();
 }
